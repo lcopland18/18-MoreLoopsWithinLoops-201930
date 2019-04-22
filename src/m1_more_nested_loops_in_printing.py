0324dbed5bd008f5c30ed3f7113978f87129043e
lcopland18 <coplanlm@rose-hulman.edu>
2019-04-22 08:45:48 -0400

    ``

--- src/m1_more_nested_loops_in_printing.py
+++ src/m1_more_nested_loops_in_printing.py
@@ -4,8 +4,8 @@ in the context of PRINTING on the CONSOLE.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Lauren Copland.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -75,6 +75,17 @@ def triangle_right_justified(r):
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
 
+    # for k in range(r):
+    #     for j in range(r):
+    #         print((' ')*(r-(k+1)),(j+1),end='')
+    #     print('')
+
+    for k in range(r):
+        for j in range(r-(k+1)):
+            print((' '),end='')
+        for i in range(k+1):
+            print(i+1, end = '')
+        print('')
 
 def run_test_triangle_upside_down():
     """ Tests the    triangle_upside_down    function. """
@@ -169,6 +180,10 @@ def vee(r):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
+
+    # for k in range():
+    #     for k in range():
+    #     for i
 
 
 def run_test_numbers_constant_forward():
